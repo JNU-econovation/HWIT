65cd945549e0711a763a200ea08bf1eaf96c5747
jbj616 <jbj616@gmail.com>
2019-05-01 21:31:56 +0900

    api 수정

--- 병재/api.js
+++ 병재/api.js
@@ -1,4 +1,4 @@
-var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo.json';
+var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo';
 var numOfRows = '10';
 var pageNo = '1';
 var depPlaceId = 'NAT010000';
@@ -7,11 +7,10 @@ var depPlandTime = '20161001';
 var trainGradeCode = '00';
 var serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
 
-var total = url+"?"+"serviceKey="+serviceKey+"&numOfRows="+numOfRows+"&pageNo="
+var totalUrl = url+"?"+"serviceKey="+serviceKey+"&numOfRows="+numOfRows+"&pageNo="
             +pageNo+"&depPlaceId="+depPlaceId+"&arrPlaceId="+arrPlaceId+"&depPlandTime="
             +depPlandTime+"&trainGradeCode="+trainGradeCode;
 
-var totalUrl = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D&numOfRows=10&pageNo=1&depPlaceId=NAT010000&arrPlaceId=NAT011668&depPlandTime=20161001&trainGradeCode=00';
 var request = require('request');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
@@ -22,7 +21,7 @@ request({
 }, function (error, response, body) {
 
   parser.parseString(body, function(err, result) {
-    var obj = result.response.body[0].items[0];
+    var obj = result.response.body[0].items[0].item;
     console.log(obj);
 });
 });
