ff94c41c995725cdbf83dfa88449c9b8f83dc987
jbj616 <jbj616@gmail.com>
2019-05-14 19:21:46 +0900

    add : 31-33장

--- 병재/trainCode.js
+++ 병재/trainCode.js
@@ -1,26 +1,30 @@
-var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getCtyAcctoTrainSttnList';
-var serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
+console.log(trainCode("f","Fds"));
 
-var totalUrl = url + '?' + 'serviceKey=' + serviceKey + '&cityCode=' + '11';
+function trainCode(regionCode, trainName){
+  var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getCtyAcctoTrainSttnList';
+  var serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
 
-var request = require('request');
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
-var testName = '왕십리';
-request({
-  url: totalUrl,
-  method: 'GET'
-}, function(error, response, body) {
+  var totalUrl = url + '?' + 'serviceKey=' + serviceKey + '&cityCode=' + '11';
 
-  //console.log(body);
-  parser.parseString(body, function(err, result) {
-    var obj = result.response.body[0].items[0].item;
+  var request = require('request');
+  var xml2js = require('xml2js');
+  var parser = new xml2js.Parser();
+  var testName = '왕십리';
+  var result;
+  request({
+    url: totalUrl,
+    method: 'GET'
+  }, function(error, response, body) {
+    parser.parseString(body, function(err, result) {
+      var obj = result.response.body[0].items[0].item;
 
-    for (var i = 0; i < obj.length; i++) {
-      if (obj[i].nodename[0] === testName) {
-        console.log(obj[i].nodeid[0]);
-        break;
+      for (var i = 0; i < obj.length; i++) {
+        if (obj[i].nodename[0] === testName) {
+          result =obj[i].nodeid[0];
+          break;
+        }
       }
-    }
+    });
   });
-});
+  return result;
+}
