f7903d6deac9fd8c53a6c56bce11f1f9a65e9212
jbj616 <jbj616@gmail.com>
2019-05-17 14:43:18 +0900

    feat : 구현

--- 병재/realApi.js
+++ 병재/realApi.js
@@ -1,11 +1,12 @@
 export.module = function(departure, arrival, time){
 
+  var depRegionCode = getCityCode(departure.regionName);
+  var arrRegionCode = getCityCode(arrival.regionName);
+
+  var data = getTrainTime(getTrainCode(depRegionCode), getTrainCode(arrRegionCode), time)
 
 
 }
-
-
-
 function getCityCode(regionName){ //지역 코드 가져오기
   var fs = require('fs');
 
@@ -26,8 +27,8 @@ function getTrainCode(){
   var request = require('request');
   var xml2js = require('xml2js');
   var parser = new xml2js.Parser();
-  const url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getCtyAcctoTrainSttnList';
-  const serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
+  var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getCtyAcctoTrainSttnList';
+  var serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
 
   var totalUrl = url + '?' + 'serviceKey=' + serviceKey + '&cityCode=' + '11';
 
@@ -37,17 +38,37 @@ function getTrainCode(){
     method: 'GET'
   }, function(error, response, body) {
 
-    //console.log(body);
     parser.parseString(body, function(err, result) {
       var obj = result.response.body[0].items[0].item;
 
       for (var i = 0; i < obj.length; i++) {
         if (obj[i].nodename[0] === testName) {
-          console.log(obj[i].nodeid[0]);
-          break;
+          return obj[i].nodeid[0];
         }
       }
     });
   });
+}
 
+function getTrainTime(depPlaceId, arrPlaceId, depPlandTime){
+  var trainGradeCode = '00'; //임시
+  var request = require('request');
+  var xml2js = require('xml2js');
+  var parser = new xml2js.Parser();
+  var numOfRows = 10;
+  var pageNo = 1;
+
+  var url = 'http://openapi.tago.go.kr/openapi/service/TrainInfoService/getStrtpntAlocFndTrainInfo';
+  var serviceKey = 'jxBxcZrc8JhQ7nNGuLjCrp4EzZ81v1YTowlTLBJiZdYh23K02yVU4%2BlByJ6U7v2RKLZ9FJn%2B5ORy7R3LKb%2BC5w%3D%3D';
+
+  var totalUrl = url+"?"+"serviceKey="+serviceKey+"&numOfRows="+numOfRows+"&pageNo="
+              +pageNo+"&depPlaceId="+depPlaceId+"&arrPlaceId="+arrPlaceId+"&depPlandTime="
+              +depPlandTime+"&trainGradeCode="+trainGradeCode;
+
+  request({
+    url: totalUrl,
+    method: 'GET'
+  }, function(error, response, body) {
+    return body;
+    });
 }
